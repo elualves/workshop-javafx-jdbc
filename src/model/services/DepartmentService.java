04deaead48df536cb0de9dba724a3dd04e2f9603
Eduardo Alves <elualvess@gmail.com>
2019-08-07 07:53:06 -0300

    Initializing action as parameter

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -8,7 +8,7 @@ import model.entities.Department;
 public class DepartmentService {
 
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList();
+		List<Department> list = new ArrayList<>();
 		list.add(new Department(1, "Books"));
 		list.add(new Department(2, "Computer"));
 		list.add(new Department(3, "Eletronics"));
